75f37d50752c7db5b2b3d7a9be1deb8632d6353b
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2025-02-14 14:36:48 -0300

    feat: adicionar página do painel com layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,41 @@
+'use client';
+
+import { useEffect, useState, useCallback } from 'react';
+import AddIcon from '@/components/atoms/AddIcon';
+import Button from '@/components/atoms/Button';
+import PageTitle from '@/components/molecules/PageTitle';
+import MasonryObjectives from '@/components/organisms/MasonryObjectives';
+import { OKR } from '@/interfaces/OKR';
+import { getOkrs } from '@/services/api';
+
 export default function Home() {
+  const [okrs, setOkrs] = useState<OKR[]>([]);
+
+  const fetchOkrs = useCallback(async () => {
+    const data = await getOkrs();
+    setOkrs(data);
+  }, []);
+
+  useEffect(() => {
+    fetchOkrs();
+  }, [fetchOkrs]);
+
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <>
+      <nav className="flex flex-col my-4">
+        <PageTitle>Lista de OKRs</PageTitle>
+        <div className="flex justify-end">
+          <Button variant="primary" onClick={() => { }}>
+            <div className="flex gap-2">
+              <AddIcon />
+              <span>Criar Objetivo</span>
+            </div>
+          </Button>
+        </div>
+      </nav>
+      <main>
+        <MasonryObjectives okrs={okrs} />
+      </main>
+    </>
   );
 }
