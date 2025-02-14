fba4372c92facbdb694b23a366c5a25759bcd315
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2025-02-14 13:02:26 -0300

    feat: configurar diretório e fonte para o tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,13 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      },      
+    }
+  },
   plugins: [],
 } satisfies Config;
