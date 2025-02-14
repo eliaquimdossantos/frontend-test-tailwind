eb1634a58f6728af15a7f67ba8cfb34c49e81703
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2025-02-14 14:33:08 -0300

    fix: corrijir o contraste

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -20,7 +20,7 @@ const button = tv({
       'secondary': 'bg-gray-600 border-gray-600 hover:bg-gray-500 text-dark-900',
       'danger': 'bg-red-600 border-red-600 hover:bg-red-500 text-white',
       'light': 'bg-gray-300 border-gray-300 hover:bg-gray-400 text-dark-900',
-      'outline-primary': 'bg-transparent border border-cyan-600 text-cyan-600 hover:bg-cyan-500 hover:text-white',
+      'outline-primary': 'bg-transparent border border-cyan-700 text-cyan-700 hover:bg-cyan-600 hover:text-white',
       'outline-secondary': 'bg-transparent border border-gray-600 text-gray-600 hover:bg-gray-500 hover:text-white',
       'outline-danger': 'bg-transparent border border-red-600 text-red-600 hover:bg-red-500 hover:text-white',
       'outline-light': 'bg-transparent border border-gray-200 text-gray-600 hover:bg-gray-300 hover:text-black',
