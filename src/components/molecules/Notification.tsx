099d7158703bf9450c6f53f96917671db5ef6d2b
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2025-02-15 18:24:51 -0300

    feat: adicionar context de notificações

---
+++ src/components/molecules/Notification.tsx
@@ -0,0 +1,26 @@
+import Notification from '@/interfaces/Notification';
+import { useEffect, useState } from 'react';
+
+export default function NotificationComponent({ message, variant, ...props }: Omit<Notification, 'id'>) {
+
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(true);
+    }, 10); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div
+      {...props}
+      className={`p-4 rounded-lg shadow-xl text-white transition-all duration-300 ease-in-out transform 
+      ${visible ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-5'} 
+      ${variant === 'success' ? 'bg-emerald-700' : 'bg-red-700'}`}
+    >
+      {message}
+    </div>
+  );
+}
